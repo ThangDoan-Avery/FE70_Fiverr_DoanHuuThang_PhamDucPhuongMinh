864df1c1da6766b848bb395f7bce06ad875f5dd9
PhamDucPhuongMinh <phamducphuongminh0103@gmail.com>
2021-12-31 00:16:25 +0700

    add script slick carousel

--- js/home.js
+++ js/home.js
@@ -3,6 +3,33 @@ const menuToggle = document.querySelector(".header__toggle-menu");
 const menuPhone = document.querySelector(".menu-phone");
 const menuOverlay = document.querySelector(".menu-phone .overlay");
 
+// Slick Carousel
+// when click carousel, slide is not autoplay
+// This is solution, slide is autoplay when click other window/tab
+$("#latest-news-wrapper").on(
+  "beforeChange",
+  function (event, slick, currentSlide, nextSlide) {
+    $("#latest-news-wrapper .slick-dots li")
+      .removeClass("slick-active")
+      .attr("aria-hidden", "true");
+    $("#latest-news-wrapper .slick-dots li button").focus(function () {
+      this.blur();
+    });
+  }
+);
+
+$(document).ready(function () {
+  $(".carousel__background").slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+    autoplay: true,
+  });
+});
+
+// Popup Menu
 menuPhoneItems.forEach((menuPhoneItem) => {
   menuPhoneItem.onclick = () => {
     let isOpen = menuPhoneItem.classList.contains("open");
